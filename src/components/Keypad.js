08b2b12c87ec8810a4bb3021fbf7d91ae842412f
Irene Njeru <irenenjeru8@gmail.com>
2022-05-13 08:46:52 +0300

    task completed

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
 // Code Keypad Component Here
+function Keypad(){
+    return (
+      <input 
+        onChange={ () => console.log(`Entering password...`)} 
+        style = {{ width : "200px", height : "30px"}} 
+        placeholder = "Password"
+        type= "password"
+      />
+    )
+  }
+  
+  export default Keypad;
